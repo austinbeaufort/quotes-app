23885860231b72bb73833d3ced289326c94aff7c
Austin Beaufort <tinyhatcoding@gmail.com>
2019-03-31 10:12:00 -0400

    fixing quotes

--- dist/quotes-app.e31bb0bc.js
+++ dist/quotes-app.e31bb0bc.js
@@ -244,7 +244,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57158" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58093" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -420,4 +420,4 @@ function hmrAcceptRun(bundle, id) {
   }
 }
 },{}]},{},["node_modules/parcel-bundler/src/builtins/hmr-runtime.js","index.js"], null)
-//# sourceMappingURL=quotes-app.e31bb0bc.js.map
+//# sourceMappingURL=/quotes-app.e31bb0bc.js.map
